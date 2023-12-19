24fe26ee4f882830ea12ba7556fdcaf1deb8f971
Bill Hilbert <billmakes@protonmail.com>
2023-12-19 12:26:58 -0500

    Delete nvim/.config/nvim_bak directory

--- nvim/.config/nvim_bak/lua/user/lsp/ruff-lsp.lua
+++
@@ -1,9 +0,0 @@
-require('lspconfig').ruff_lsp.setup {
-    on_attach = on_attach,
-  init_options = {
-    settings = {
-      -- Any extra CLI arguments for `ruff` go here.
-      args = {},
-    }
-  }
-}
